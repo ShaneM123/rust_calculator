d68d26ea4c7a6f0cd01b07915e7e6cf1c2ad67af
shane <moloney.shane@yahoo.com>
2020-01-14 21:25:29 +0000

    basic add functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,40 @@
 use std::io::{stdin, stdout, Write};
 
-pub fn pause()->String{
-let mut x = String::new();
-let mut stdout = stdout();
-stdout.write(b"Welcome User, please enter a number").unwrap();
-stdout.flush().unwrap();
-stdin().read_line(&mut x).expect("error, something went wrong reading input");
-x.trim().to_string()
+pub fn pause() -> String {
+    let mut x = String::new();
+    let mut stdout = stdout();
+     stdout.write(b"Welcome User, please enter a number").unwrap();
+     stdout.flush().unwrap();
+     stdin().read_line(&mut x).expect("error, something went wrong reading input");
+     x.to_string()
 }
 
-pub fn string_iterator(x: String) {
-   let mut  y = false;
-   let chr_vec = x.chars().collect();
-    for a in chr_vec{
-        if a == '+' {
-            y = true;
+pub fn string_iterator(s: String)-> i32 {
+    let mut x:i32=0;
+    let  parts = s.split_whitespace().map(|s| s.parse::<i32>());
+    for b in parts {match b {
+        Ok(a)=> {
+            x += a;
         }
+        _ => {}
+    }}
+    /*
+    match parts.next() {
+        Some(Ok(a))=> { x+=a;
+// a and b are i32
+        }
+// handle other problems: not enough numbers, numbers are invalid, etc
+        _ => {}  // ignore invalid input
 
-    }
+    }*/
+    return x
 }
-
 
 #[cfg(test)]
 mod tests {
+use super::string_iterator;
     # [test]
     fn addition_test(){
-        assert_eq!(2 + 2, 4);
+        assert_eq!(4, string_iterator("2 2".to_string()));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod lib;
 use lib::pause;
+use lib::string_iterator;
 
 fn main() {
    let x = pause();
     println!("Thank you for your participation");
     println!("{:?}", x );
+    let y = string_iterator(x);
+    println!("{:?}", y);
 
 }
